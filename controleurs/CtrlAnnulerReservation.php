7df78d5e2c6732affb9273d3cf4c3da52ffcea29
delasalle-sio-chefdor-v <delasalle.sio.chefdor.v@gmail.com>
2016-11-08 17:21:53 +0100

    Vue et controleur AnnulerReservation

---
+++ controleurs/CtrlAnnulerReservation.php
@@ -0,0 +1,62 @@
+<?php
+// Projet Réservations M2L - version web mobile
+// fichier : vues/VueAnnulerReservations.php
+// Rôle : supprimer une réservation en entrant le numéro de la réservation	// cette vue est appelée par le contôleur controleurs/CtrlAnnulerReservation.php
+// Création : 08/11/2016 par Chefdor
+// Mise à jour : 08/11/2016 par Chefdor
+	
+// on vérifie si le demandeur de cette action est bien authentifié
+if ( $_SESSION['niveauUtilisateur'] != 'utilisateur' && $_SESSION['niveauUtilisateur'] != 'administrateur') {
+	// si le demandeur n'est pas authentifié, il s'agit d'une tentative d'accès frauduleux
+	// dans ce cas, on provoque une redirection vers la page de connexion
+	header ("Location: index.php?action=Deconnecter");
+}
+else {
+	// connexion du serveur web à la base MySQL
+	
+	if ( ! isset ($_POST ["txtAnnulerReservation"])) {
+		// si les données n'ont pas été postées, c'est le premier appel du formulaire : affichage de la vue sans message d'erreur
+		$numeroReservation = '';
+		$message = '';
+		$typeMessage = '';			// 2 valeurs possibles : 'information' ou 'avertissement'
+		$themeFooter = $themeNormal;
+		include_once ('vues/VueAnnulerReservation.php');
+	}
+	else {
+		// récupération des données postées
+		if ( empty ($_POST ["txtAnnulerReservation"]) == true)  $numeroReservation = "";  else   $numeroReservation = $_POST ["txtAnnulerReservation"];
+		
+		if ($numeroReservation == '') {
+			// si les données sont incomplètes, réaffichage de la vue avec un message explicatif
+			$message = 'Données incomplètes ou incorrectes !';
+			$typeMessage = 'avertissement';
+			$themeFooter = $themeProbleme;
+			include_once ('vues/VueAnnulerReservation.php');
+		}
+		else {
+			// connexion du serveur web à la base MySQL
+			include_once ('modele/DAO.class.php');
+			$dao = new DAO();
+			
+			// test de la réservation de l'utilisateur
+			$_SESSION['nom'] = $nom;
+			// la méthode estLeCreateur de la classe DAO retourne les valeurs true ou false celon si la réservation correspond ou non à l'utilisateur
+			if ($dao->estLeCreateur($nom, $numeroReservation) == false){
+				$message = 'Ce numéro de réservation ne correspond pas à une de vos réservation !';
+				$typeMessage = 'avertissement';
+				$themeFooter = $themeProbleme;
+				include_once ('vues/VueAnnulerReservation.php');
+			}
+			else{
+				$dao->annulerReservation($numeroReservation);	
+			}
+	
+			
+			unset($dao);		// fermeture de la connexion à MySQL
+		}
+	}
+	
+	unset($dao);		// fermeture de la connexion à MySQL
+}
+
+?>
---
+++ vues/VueAnnulerReservation.php
@@ -0,0 +1,35 @@
+<?php
+	// Projet Réservations M2L - version web mobile
+	// fichier : vues/VueAnnulerReservations.php
+	// Rôle : supprimer une réservation en entrant le numéro de la réservation
+	// cette vue est appelée par le contôleur controleurs/CtrlAnnulerReservation.php
+	// Création : 08/11/2016 par Chefdor
+	// Mise à jour : 08/11/2016 par Chefdor
+?>
+<!doctype html>
+<html>
+	<head>
+		<?php include_once ('vues/head.php'); ?>
+	</head>
+	 
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
+				<h4>M2L-GRR</h4>
+				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
+			</div>
+			<div data-role="content">
+				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Annuler une réservation</h4>
+				<form action="index.php?action=AnnulerReservation" method="post" data-ajax="false">
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<label for="txtAnnulerReservation">Réservation</label>
+						<input type="text" name="txtName" id="txtName" required placeholder="Entrez le numéro de réservation" value="<?php echo $name; ?>">
+					</div>
+					<div data-role="fieldcontain">
+						<input type="submit" name="btnAnnulerReservation" id="btnAnnulerReservation" value="Annuler la réservation" data-mini="true">
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
